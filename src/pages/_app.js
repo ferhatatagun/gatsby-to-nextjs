d097bdc0bdcefd9ecd0ec28bbae7c930f019a0e0
Ferhat Atagün <ferhatatagun2@gmail.com>
2022-11-25 01:15:56 +0300

    The globals.css file has been moved to app.js.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,6 @@
+// CSS not modular here to provide global styles
+import '../assets/Globals.css';
+
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
